a0c0601bdbfc743c564a37e7f611675144111cb6
Ji Ho Choi <withj1123@gmail.com>
2018-06-01 16:32:24 +0900

    config slight change

---
+++ client/src/components/Navigations/TopNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {withStyles} from 'material'
+
+class TopNav extends React.Component{
+    render(){
+        const {classes} = this.props;
+
+        return (
+            <div className={classes.root}>
+
+            </div>
+        )
+    }
+}
--- configs/default.js
+++ configs/default.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT:8000,
     SESSION_LIFE:null,
-    SESSION_SECRET:'veryverychorongparkchorong',
-    MONGO_URI:'mongodb://localhost/ReactEcommerceMaterialUi',
+    SESSION_SECRET: process.ENV.SESSION_SECRET,
+    MONGO_URI:process.ENV.MONGO_URI,
 
 };
