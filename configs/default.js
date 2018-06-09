fa7a94c430e4ef23ed8e11f5303d5f3b04756c6d
Ji Ho Choi <withj1123@gmail.com>
2018-06-09 17:12:17 +0900

    packages update

--- configs/default.js
+++ configs/default.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: 8000,
     SESSION_LIFE: null,
-    SESSION_SECRET: process.ENV.SESSION_SECRET,
-    MONGO_URI: process.ENV.MONGO_URI,
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    MONGO_URI: process.env.MONGO_URI,
 
 };
